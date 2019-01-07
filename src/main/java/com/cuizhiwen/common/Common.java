a74f33d78ac1b6b2640df8eee9d51860230acb66
cuizhiwen <01418061@yto.net.cn>
2019-01-07 13:12:01 +0800

    提交

---
+++ src/main/java/com/cuizhiwen/common/Common.java
@@ -0,0 +1,19 @@
+package com.cuizhiwen.common;
+
+/**
+ * @author 01418061(cuizhiwen)
+ * @Description:
+ * @date 2019/1/7 10:52
+ */
+public class Common {
+    /**
+     * 1>GroupID 是项目组织唯一的标识符，实际对应JAVA的包的结构，是main目录里java的目录结构。
+     * 2>ArtifactID是项目的唯一的标识符，实际对应项目的名称，就是项目根目录的名称。
+     *
+     * 3>apolication/yml 配置文件，用于数据库连接，服务器接口设置
+     * 4>pom.xml mavne构建项目的配置文件，定义了项目基本信息及依赖，插件等。
+     *
+     * 5> alt+insert 生成代码   ctrl+alt+s 设置页面  ctrl+alt+T 生成try  ctrl+o重写 ctrl+I实现
+     *    ctrl+alt+l 格式化代码
+     */
+}
---
+++ src/main/java/com/cuizhiwen/common/Config.java
@@ -0,0 +1,15 @@
+package com.cuizhiwen.common;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 01418061(cuizhiwen)
+ * @Description:
+ * @date 2019/1/7 10:34
+ */
+@Configuration
+public class Config {
+    /**
+     * 1> @Configuration 是最新的用注解配置spring，也就是说这是个配置文件，和原来xml配置是等效的
+     */
+}
