bfa90c99ef864b73f38761b3128dfe4cecd1dec6
cuizhiwen <01418061@yto.net.cn>
2019-01-07 13:11:50 +0800

    提交

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,4 +1,10 @@
+import com.cuizhiwen.common.Config;
+import com.cuizhiwen.common.Controller;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 /**
  * @author 01418061(cuizhiwen)
@@ -6,9 +12,35 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
  * @date 2019/1/7 10:00
  */
 @EnableAutoConfiguration
+@ComponentScan
+    @ComponentScan(value = "com.cuizhiwen.common", includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Controller.class})})
 public class app {
     /**
      * 1> @EnableAutoConfiguration 告诉容器去通过依赖的jar配置spring工程，
      *    例如:spring-boot-starter-web中包含的tomcat和springmvc，它会认为这是一个web项目并进行相应的配置。
+     *
+     * 2> @ComponentScan告诉Spring 哪个packages 的用注解标识的类 会被spring自动扫描并且装入bean容器，包括所有的配置类。
+     *            1)创建一个配置类，在配置类上添加 @ComponentScan 注解。该注解默认会扫描该类所在的包下所有的配置类
+     *            2）指定要扫描的包（使用@ComponentScan 的 valule 属性来配置）
+     *            3）使用 excludeFilters 来按照规则排除某些包的扫描。
+     *            4) @ComponentScan 的 useDefaultFilters 属性，该属性默认值为 true，spring 默认会自动发现被
+     *               @Component、@Repository、@Service 和 @Controller 标注的类，并注册进容器中。
+     *            5)添加多种扫描规则 ,可以添加多个 @ComponentScan 来添加多个扫描规则，但是在配置类中要加上 @Configuration 注解，否则无效。
+     *
      */
+
+    /**
+     * 获取已经注册到容器中的 bean 的名称
+     * @param args
+     */
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(Config.class);
+
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        for (String beanName : beanDefinitionNames) {
+            System.out.println("**********beanName************: " + beanName);
+        }
+    }
+
 }
--- src/main/java/com/cuizhiwen/common/Controller.java
+++ src/main/java/com/cuizhiwen/common/Controller.java
@@ -10,12 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
     /**
-     * 1>GroupID 是项目组织唯一的标识符，实际对应JAVA的包的结构，是main目录里java的目录结构。
-     *
-     * 2>ArtifactID是项目的唯一的标识符，实际对应项目的名称，就是项目根目录的名称。
-     *
      * 1> @@RestController 无法返回jsp或html中配置的视图解析器，返回json，xml或自定义内容。
      * 2> @Controller 可解析return的jsp，html并跳到响应的页面。
-     * 3>
+     * 3> @Component  在持久层、业务层和控制层分别采用 @Repository、@Service 和 @Controller 对分层中的类进行注释，
+     *                而用 @Component 对那些比较中立的类进行注释。
      */
 }
