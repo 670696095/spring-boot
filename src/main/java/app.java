97016fe93a3749ed4f7a081f63247340c96ff92f
cuizhiwen <01418061@yto.net.cn>
2019-01-07 10:23:46 +0800

    提交

---
+++ src/main/java/app.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * @author 01418061(cuizhiwen)
+ * @Description:
+ * @date 2019/1/7 10:00
+ */
+@EnableAutoConfiguration
+public class app {
+    /**
+     * 1> @EnableAutoConfiguration 告诉容器去通过依赖的jar配置spring工程，
+     *    例如:spring-boot-starter-web中包含的tomcat和springmvc，它会认为这是一个web项目并进行相应的配置。
+     */
+}
